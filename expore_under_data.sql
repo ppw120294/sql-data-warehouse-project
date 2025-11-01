9537181ef6c67a7ac917af246b1bfe50de32b507
PhyuPhyu <57343794+ppw120294@users.noreply.github.com>
2025-11-01 17:44:50 +0100

    Delete expore_under_data.sql

--- expore_under_data.sql
+++
@@ -1,9 +0,0 @@
-SELECT TOP 1000 * FROM bronze.crm_cust_info;
-SELECT TOP 1000 * FROM bronze.crm_prd_info;
-SELECT TOP 1000 * FROM bronze.crm_sales_details;
-
-SELECT TOP 1000 * FROM bronze.erp_cust_az12;
-SELECT TOP 1000 * FROM bronze.erp_loc_a101;
-SELECT TOP 1000 * FROM bronze.erp_px_cat_g1v2;
-
-      
